8d7f3ac448374b3af6d13fcd037b0feb3eb9dfd7
Mmb <medmehdibelghali@gmail.com>
2021-03-01 12:08:37 +0100

    Commit Message 5 ?

---
+++ src/main/TestMainMaster.java
@@ -0,0 +1,10 @@
+package main;
+
+public class TestMainMaster {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This class should be visible in master branch only before the merge");
+	}
+
+}
