1225d10113ec094a826fafbca1ac398dd6583b26
Mmb <medmehdibelghali@gmail.com>
2021-03-01 12:02:21 +0100

    A simple Commit and Push 4

---
+++ src/main/TestMainGreetingServices.java
@@ -0,0 +1,7 @@
+package main;
+
+public class TestMainGreetingServices {
+	public static void main(String[] args) {
+		System.out.println("This Class is only visible in the GreetingServices Branch");
+	}
+}
